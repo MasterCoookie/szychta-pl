a26b6269454000d8092cea08edfae2f8ea2046ab
MasterCoookie <jas.kocurek2@wp.pl>
2023-04-07 14:53:32 +0200

    session and secret

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const session = require('express-session');
+const secret = require('./secret');
 
 const app = express();
 
@@ -10,6 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(port, () => {
     console.log('szychta.pl listening for reqests on port ' + port);
 });
+
+app.use(
+	session({
+		secret: secret.COOKIE_SECRET,
+		resave: false,
+		//change last number to specify session max age in hours
+		cookie: { maxAge: 1000 * 3600 * 1 },
+		saveUninitialized: false,
+		// store
+	})
+);
 
 app.get('/', (req, res) => {
     res.send("szychta.pl");
