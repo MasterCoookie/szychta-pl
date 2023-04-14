84cd6222063695733b949fd4cfc34ed23d8de725
MasterCoookie <jas.kocurek2@wp.pl>
2023-04-14 17:32:31 +0200

    applicant model created

---
+++ models/apllicantModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const { isEmail, isMobilePhone, isUrl } = require('validator');
+
+const applicantModel = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    surname: {
+        type: String,
+        required: true
+    },
+    uploadedDocuments: [String],
+    email: {
+        type: String,
+        required: true,
+        validate: [isEmail, "Email invalid"]
+    },
+    phoneNumber: {
+        type: String,
+        validate: isMobilePhone
+    },
+    birthDate: {
+        type: Date,
+    },
+    homeAddress: {
+        type: String,
+    },
+    links: {
+        type: [String],
+        //TODO validate
+        // validate: v=> v.forEach(link => {
+        //   return isUrl(link);  
+        // })
+    },
+});
+
+//add your model mehtods here
+
+const Applicant = mongoose.model('Applicant', applicantModel);
+
+module.exports = Applicant;
