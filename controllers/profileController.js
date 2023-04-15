c8ac0e62134a3f87584cd472c9a59aba730f7cd0
MasterCoookie <jas.kocurek2@wp.pl>
2023-04-15 13:36:17 +0200

    error handling bugfixes

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -14,17 +14,21 @@ const profile_post = async (req, res) => {
         if (e.code === 11000) {
             errors.push('Email already in use')
         }
-        Object.values(e.errors).forEach(({ properties }) => {
-            if (properties.message) {
-                errors.push(properties.message);
-            }
-        });
-        console.log(e);
-        res.sendStatus(400).json(errors);
+        if(e.errors) {
+            e.errors.forEach(({ properties }) => {
+                if (properties.message) {
+                    errors.push(properties.message);
+                }
+            });
+        }
+        
+        // console.log(e);
+        res.status(400).json(errors);
     }
 }
 const profile_get = async (req, res) => {
     try {
+        //TMP DUMMY DATA
         const applicant = new Applicant({ name: 'dupa', surname: 'dupa2', email: 'pat.i.mat233@gmail.com', phoneNumber: '224444444', birthDate: '12.12.2001', homeAdress: 'kasztanowa 52 lipinki łużyckie', links: ['patrzuwa.ga', 'macibol.ga'] });
         await applicant.save(applicant);
         res.sendStatus(200);
