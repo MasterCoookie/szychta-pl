c8ab58b29046e6e55d213a98b86588a89a6b0303
MasterCoookie <jas.kocurek2@wp.pl>
2023-04-17 20:23:39 +0200

    script work in progress

---
+++ public/js/varyingSizeInputArray.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', function() {
+
+    const inputTemplate = '<input type="text" id="link" />';
+    const inputArrayDOMElement = document.getElementById('inputArray');
+    let arrayAddDOMElement = document.getElementById('arrayAdd');
+    let inputArray = [];
+
+    arrayAddDOMElement.addEventListener('click', function(e) {
+        e.preventDefault();
+        console.log('dupa');
+        inputArray.push(inputTemplate);
+        renderInputArray(inputArray);
+    });
+
+
+    function renderInputArray(inputArray) {
+
+        if(inputArray.length < 8) {
+            arrayAddDOMElement.style.display = 'inline';
+        } else {
+            arrayAddDOMElement.style.display = 'none';
+        }
+
+        inputArrayDOMElement.innerHTML = inputArray.join('') + inputArrayDOMElement.innerHTML;
+    }
+
+
+    
+    renderInputArray(inputArray);
+})
+
