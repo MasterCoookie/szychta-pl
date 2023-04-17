1ebe8f27038d8bbc8047049e302f39e7c7fe41f0
MasterCoookie <jas.kocurek2@wp.pl>
2023-04-17 21:02:57 +0200

    elements adding bugfix

--- public/js/varyingSizeInputArray.js
+++ public/js/varyingSizeInputArray.js
@@ -3,36 +3,33 @@ window.addEventListener('load', function() {
     const getInputTemplate = (index) => '<input type="text" id="link" data-index="' + index + '" /><button type="button" data-index="' + index + '" class="array-remove">x</button>';
     const inputArrayDOMElement = document.getElementById('inputArray');
     let arrayAddDOMElement = document.getElementById('arrayAdd');
-    let inputArray = [getInputTemplate(0)];
+    let inputArrayLen = 1;
 
-    function renderInputArray(inputArray) {
+    function renderInputArray() {
 
-        if(inputArray.length < 8) {
+        if(inputArrayLen < 8) {
             arrayAddDOMElement.style.display = 'inline';
         } else {
             arrayAddDOMElement.style.display = 'none';
         }
 
-        inputArrayDOMElement.innerHTML = inputArray.join('');
 
         const arrayRemoveDOMElements = Array.from(document.getElementsByClassName("array-remove"));
 
         arrayRemoveDOMElements.forEach(element => {
             element.addEventListener('click', function(e) {
                 const clickedIndex = e.target.dataset.index;
-                inputArray.splice(clickedIndex, 1);
-                renderInputArray(inputArray)
+                console.log(clickedIndex);
             });
         });
     }
     
-    renderInputArray(inputArray);
-
-    const arrayRemoveDOMElement = document.getElementById('arrayRemove');
+    renderInputArray();
 
     arrayAddDOMElement.addEventListener('click', function(e) {
         e.preventDefault();
-        inputArray.push(getInputTemplate(inputArray.length));
-        renderInputArray(inputArray);
+        inputArrayLen++;
+        inputArrayDOMElement.insertAdjacentHTML('afterend', getInputTemplate(inputArrayLen));
+        renderInputArray();
     });
 });
