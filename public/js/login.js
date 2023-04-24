2b12039bf024c7d954a10fc24b76f02ed154a6ec
tomazaw <tomazaw886@student.polsl.pl>
2023-04-24 23:53:25 +0200

    Login page styling and moved login script to an external file

---
+++ public/js/login.js
@@ -0,0 +1,21 @@
+const loginForm = document.getElementById('loginForm');
+const loginButton = document.getElementById('loginBtn');
+
+loginButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const request = new XMLHttpRequest();
+    const formData = new FormData(loginForm);
+
+    request.addEventListener('load', (event) => {
+        const response = JSON.parse(event.target.responseText);
+
+        if(response.redirect === 'profile'){
+            window.location.href = '/profile';
+        } else if(response.msg !== null){
+            document.getElementById('message').innerHTML = response.msg;
+        }
+    });
+    
+    request.open('post', '/auth/login');
+    request.send(formData);
+});
