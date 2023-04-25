95d3726d173619cd951db8e5aa189ca988a2176e
macibol775 <macibol775@student.polsl.pl>
2023-04-25 21:37:24 +0200

    split routers into 2

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const profileController = require('../controllers/profileController');
-const jobOfferController = require('../controllers/jobOfferController');
 const uploadMiddleware = require('../middleware/uploadMiddleware');
 const multer = require('multer');
 
@@ -25,12 +24,5 @@ router.get('/profile', profileController.profile_get);
 router.post('/profile', profileController.profile_post);
 router.delete('/profile/file_delete', upload.none(), profileController.file_delete);
 
-// job offer related routes
-router.get('/employer/show_offers', jobOfferController.showOffers_get);
-router.get('/employer/add_offer', jobOfferController.addOffer_get);
-router.post('/employer/add_offer', upload.none(), jobOfferController.addOffer_put);
-router.get('/employer/modify_offer', jobOfferController.modifyOffer_get);
-router.post('/employer/modify_offer', upload.none(), jobOfferController.modifyOffer_post);
-router.delete('/employer/delete_offer', upload.none(), jobOfferController.offer_delete);
 
 module.exports = router;
