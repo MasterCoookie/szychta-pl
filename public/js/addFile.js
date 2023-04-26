8f988a73c2b32b31575e31e2a5320bcdc98b7531
WojciechPtas <wojciech.ptas@gmail.com>
2023-04-26 20:37:58 +0200

    playing around with js

--- public/js/addFile.js
+++ public/js/addFile.js
@@ -1,5 +1,7 @@
 window.addEventListener('load', function () {
+    let currentIndex = 1;
     const FileToAddDOMElement = document.getElementById('addfile');
+    const getInputTemplate = (index,name) => name + '<button type="button" data-buttonindex="" class="files-remove" id="' + index + '">x</button>';
     FileToAddDOMElement.addEventListener('change', function (e) {
         const file = e.target.files[0];
         console.log("dupa");
@@ -12,6 +14,11 @@ window.addEventListener('load', function () {
         new_request = new XMLHttpRequest();
         new_request.open('GET', '/profile/docs');
         new_request.send();
-        new_request.onload = () => alert(new_request.response);
+        new_request.onload = () => {
+            const array = JSON.parse(new_request.response);
+            array.forEach(element => {
+            FileToAddDOMElement.insertAdjacentHTML('beforeend', getInputTemplate(currentIndex - 1,element));
+        });
+        };
     });
 });
