78a985d08ce6a98135b1568da126dc2ab8c672c6
MasterCoookie <jas.kocurek2@wp.pl>
2023-04-29 18:27:12 +0200

    search firing functional

---
+++ public/js/skillsSearch.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', function () {
+    const searchBarDOMElement = document.getElementById('skillsSearchBar');
+    const searchResultsDOMElement = document.getElementById('skillsResults');
+    let timeout = null;
+
+    searchBarDOMElement.addEventListener('input', function (e) {
+        const searchQuery = e.target.value;
+        if(searchQuery.length == 0) {
+            searchResultsDOMElement.innerHTML = 'Start typing to search for skills';
+            return;
+        }
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+            console.log("Searching for: " + searchQuery);
+        }, 500);
+    });
+});
