ebbf2931e016d7dd6c75fb7579b9cb97dd12d064
macibol775 <macibol775@student.polsl.pl>
2023-05-01 00:36:18 +0200

    most done; todo: handle date copy, separate put and post

--- controllers/jobOfferController.js
+++ controllers/jobOfferController.js
@@ -27,8 +27,9 @@ const showOffers_get = async (req, res) => {
 }
 
 const addOffer_put = async (req, res) => {
-    const { title, description, requirements, salary, location, industry, keywords, expiryDate, organisation_id, mode0, mode1, mode2} = req.body;
-    let additionalQuestions = []; //temp
+    const { title, description, requirements, salary, location, industry, expiryDate, organisation_id, mode0, mode1, mode2} = req.body;
+    const additionalQuestions = req.body.additionalQuestions ? JSON.parse(req.body.additionalQuestions) : [];
+    const keywords = req.body.keywords ? JSON.parse(req.body.keywords) : [];
     let mode = await getModeArray(mode0, mode1, mode2);
     try {
         const jobOffer = await JobOffer.create({ title, description, mode, salary, requirements, location, industry, additionalQuestions, keywords, expiryDate, organisation_id});
--- public/js/modifyOffer.js
+++ public/js/modifyOffer.js
@@ -5,10 +5,17 @@ submitButton.addEventListener('click', (event) => {
     event.preventDefault();
     const request = new XMLHttpRequest();
     const formData = new FormData(form);
+    const keywordsArrayInputs = Array.from(document.getElementsByClassName('keywords-array-input'));
+    const keywordsArrayValues = JSON.stringify(keywordsArrayInputs.map(input => input.value));
+    formData.append('keywords', keywordsArrayValues);
+    const addQuestionsArrayInputs = Array.from(document.getElementsByClassName('additionalQuestions-array-input'));
+    const addQuestionsArrayValues = JSON.stringify(addQuestionsArrayInputs.map(input => input.value));
+    formData.append('additionalQuestions', addQuestionsArrayValues);
     console.log(formData);
     request.onload = () => {
         console.log(request.response);
     }
     request.open('put', '/employer/add_offer');
     request.send(formData);
+    //todo check if is modified - then post
 });
