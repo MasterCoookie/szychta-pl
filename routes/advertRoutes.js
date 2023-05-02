04c94636d3eacd71b156eb4efeffac1c3f48c697
WojciechPtas <wojciech.ptas@gmail.com>
2023-05-02 20:22:14 +0200

    rename var

--- routes/advertRoutes.js
+++ routes/advertRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const jobAdvertRoute = require('../controllers/jobAdvertController');
+const jobAdvertController = require('../controllers/jobAdvertController');
 
 const router = express.Router();
 
-router.get('/view', jobAdvertRoute.showAdvert);
+router.get('/view', jobAdvertController.showAdvert);
 
 module.exports = router;
