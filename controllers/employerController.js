cd4ffbb3dc9e2c51ee916312806f16f1e32cf060
bartorl <bartorl153@student.polsl.pl>
2023-05-04 19:46:57 +0200

    Need push

--- controllers/employerController.js
+++ controllers/employerController.js
@@ -2,10 +2,8 @@ const Employer = require('../models/empolyerModel');
 
 const panel_get = (req, res) => {
     if(req.session.authenticated){
-        console.log('Redirect')
         res.render('employer/employerPanel', { title: 'employerPanel' });
     } else {
-        console.log('Not authenticated')
         res.redirect('/');
     }
 };
