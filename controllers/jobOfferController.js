81a29d622e1147436bdf748893ff03497b2c94ff
Wojciech Siudy <wojcsiu541@student.polsl.pl>
2023-05-04 17:36:52 +0200

    Fix jobOffer query\n\nBad part of request was passed to database

--- controllers/jobOfferController.js
+++ controllers/jobOfferController.js
@@ -29,9 +29,9 @@ const showOffers_get = async (req, res) => {
 const showOfferDetails_get = async (req, res) => {
     try {
         console.log(req.query);
-        const jobOffer = await JobOffer.findById(req.query.id);
+        const jobOffer = await JobOffer.findById(req.query._id);
         console.log(jobOffer);
-        res.render('jobOffer/show_offer_details', { title: 'Show offer', jobOffer });
+        res.render('jobOffer/show_offer_details', { title: 'Show offer details', jobOffer });
     }
     catch (e) {
         console.log(e);
