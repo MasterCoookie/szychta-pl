c85b0c67f4428053b23f18e9a6a727e58a5c9c5c
tomazaw <tomazaw886@student.polsl.pl>
2023-05-09 23:45:18 +0200

    Adds application viewing from the applicant side

--- routes/applicantRoutes.js
+++ routes/applicantRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const jobOfferController = require('../controllers/jobOfferController');
 const applyController = require('../controllers/applyController');
 const authMiddleware = require('../middleware/authMiddleware');
+const applicationController = require('../controllers/applicationController');
 const multer = require('multer');
 
 const upload = multer();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get('/view', jobOfferController.showOfferDetails_get);
 router.get('/apply', authMiddleware.require_login , applyController.showApplyingForm);
 router.post('/apply', upload.none() , applyController.apply_post);
+router.get('/show_applications', authMiddleware.require_login , applicationController.show_applicant_applications);
 
 module.exports = router;
