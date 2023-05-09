0a6f2c64f64a5f2de473a8be26318b83d2c2ef5b
tomazaw <tomazaw886@student.polsl.pl>
2023-05-10 00:26:26 +0200

    Changes page titles and controller function name

--- routes/applicantRoutes.js
+++ routes/applicantRoutes.js
@@ -12,6 +12,6 @@ const router = express.Router();
 router.get('/view', jobOfferController.showOfferDetails_get);
 router.get('/apply', authMiddleware.require_login , applyController.showApplyingForm);
 router.post('/apply', upload.none() , applyController.apply_post);
-router.get('/show_applications', authMiddleware.require_login , applicationController.show_applicant_applications);
+router.get('/show_applications', authMiddleware.require_login , applicationController.show_applicant_applications_get);
 
 module.exports = router;
