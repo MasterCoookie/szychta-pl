dd02ae693e04aa652124731f0a3a01fb38f320d7
WojciechPtas <wojciech.ptas@gmail.com>
2023-05-10 09:45:03 +0200

    created routes for admin

---
+++ routes/adminRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const adminController = require('../controllers/adminController');
+const authMiddleware = require('../middleware/authMiddleware');
+const multer = require('multer');
+
+const router = express.Router();
+const upload = multer();
+
+router.put('/add_organisation', upload.none(), adminController.addOrganisation_put);
+router.post('/modify_organisation', adminController.modifyOrganistation_post);
+router.delete('/delete_organisation', upload.none(), adminController.organisation_delete);
+
+router.put('/add_employer', upload.none(), adminController.addEmployer_put);
+router.post('/modify_employer', adminController.modifyEmployer_post);
+router.delete('/delete_employer', upload.none(), adminController.employer_delete);
+
+router.get('/employerPanel',authMiddleware.require_login, adminController.panel_get);
+
+
+module.exports = router;
