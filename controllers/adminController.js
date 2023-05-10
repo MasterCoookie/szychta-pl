ecafd229be0dc66e5803d98fe30c7116cb56e810
WojciechPtas <wojciech.ptas@gmail.com>
2023-05-10 09:45:21 +0200

    create empty controllers

---
+++ controllers/adminController.js
@@ -0,0 +1,45 @@
+const Employer = require('../models/employerModel');
+const Organisation = require('../models/organisationModel');
+// Should this be splitted into employerController and organisationController?
+const manageEmployer_get = async (req, res) => {
+
+}
+const manageOrganisation_get = async (req, res) => {
+
+}
+const addOrganisation_put = async (req, res) => {
+
+}
+const addEmployer_put = async (req, res) => {
+
+}
+const modifyOrganistation_post = async (req, res) => {
+
+}
+
+const modifyEmployer_post = async (req, res) => {
+
+}
+
+const employer_delete = async (req, res) => {
+
+}
+
+const organisation_delete = async (req, res) => {
+
+}
+
+const panel_get = async (req, res) => {
+
+}
+module.exports = {
+    manageEmployer_get,
+    addEmployer_put,
+    modifyEmployer_post,
+    employer_delete,
+    manageOrganisation_get,
+    addOrganisation_put,
+    modifyOrganistation_post,
+    organisation_delete,
+    panel_get
+}
