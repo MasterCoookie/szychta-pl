b4de336cf947a8ab9477f3d262d789569db34d65
MasterCoookie <jas.kocurek2@wp.pl>
2023-05-10 16:52:09 +0200

    authMiddleware expanded for higher permission lvls

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -6,6 +6,33 @@ const require_login = (req, res, next) => {
     }
 }
 
+const require_secretary = (req, res, next) => {
+    if(req.session.employer && req.session.employer.level >= 1) {
+        next();
+    } else {
+        res.redirect('/');
+    }
+}
+
+const require_recruiter = (req, res, next) => {
+    if(req.session.employer && req.session.employer.level >= 2) {
+        next();
+    } else {
+        res.redirect('/');
+    }
+}
+
+const require_admin = (req, res, next) => {
+    if(req.session.employer && req.session.employer.level >= 3) {
+        next();
+    } else {
+        res.redirect('/');
+    }
+}
+
 module.exports = {
     require_login,
+    require_secretary,
+    require_recruiter,
+    require_admin
 }
