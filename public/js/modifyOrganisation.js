b09fbe5d5f73820d8b5c148be23f02c9bf58e870
WojciechPtas <wojciech.ptas@gmail.com>
2023-05-10 16:23:51 +0200

    js helper functions to send forms

---
+++ public/js/modifyOrganisation.js
@@ -0,0 +1,22 @@
+function initializeModifyOrganisationSubmitListener (organisation_id){
+    const form = document.getElementById('offerForm');
+    const submitButton = document.getElementById('submitBtn');
+
+    submitButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        const request = new XMLHttpRequest();
+        const formData = new FormData(form);
+        request.onload = () => {
+            console.log(request.response);
+        }
+        if (organisation_id){
+            formData.append('organisation_id', organisation_id);
+            request.open('post', '/admin/modify_organisation');
+        } else {
+            request.open('put', '/admin/add_organisation');
+        }
+        request.send(formData);
+    });
+}
+
+export { initializeModifyOrganisationSubmitListener as initializeModifyOrganisationSubmitListener };
