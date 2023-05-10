cfb32815905a621ac0ac776c91c52e19274827a4
WojciechPtas <wojciech.ptas@gmail.com>
2023-05-10 14:25:04 +0200

    added manage routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -8,13 +8,15 @@ const upload = multer();
 
 router.put('/add_organisation', upload.none(), adminController.addOrganisation_put);
 router.post('/modify_organisation', adminController.modifyOrganistation_post);
+router.get('/manage_organisation', adminController.manageOrganisation_get);
 router.delete('/delete_organisation', upload.none(), adminController.organisation_delete);
 
 router.put('/add_employer', upload.none(), adminController.addEmployer_put);
 router.post('/modify_employer', adminController.modifyEmployer_post);
+router.get('manage_employer', adminController.manageEmployer_get);
 router.delete('/delete_employer', upload.none(), adminController.employer_delete);
 
-router.get('/employerPanel',authMiddleware.require_login, adminController.panel_get);
+router.get('/adminPanel',authMiddleware.require_login, adminController.panel_get);
 
 
 module.exports = router;
