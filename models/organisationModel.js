65d2c0367db655d522b92ae3795d79d6591c3170
WojciechPtas <wojciech.ptas@gmail.com>
2023-05-10 09:43:55 +0200

    created organisation mode;

---
+++ models/organisationModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const organisationSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a name for an organisation"],
+        maxlength: [256, 'Name too long']
+    },
+    description: {
+        type: String,
+        required: false, 
+        maxlength: [1024, 'Description too long']
+    },
+});
+
+const Organisation = mongoose.model('Organisation', organisationSchema);
+
+module.exports = Organisation;
