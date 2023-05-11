494425bac1fef1a8b80f4aba60bcd8b9ae786a72
WojciechPtas <wojciech.ptas@gmail.com>
2023-05-11 10:03:01 +0200

    handling responses

--- public/js/modifyOrganisation.js
+++ public/js/modifyOrganisation.js
@@ -1,13 +1,27 @@
 function initializeModifyOrganisationSubmitListener (organisation_id){
     const form = document.getElementById('orgForm');
     const submitButton = document.getElementById('submitBtn');
+    const organisationSaveResultDOMElement = document.getElementById('organisationSaveResult');
 
     submitButton.addEventListener('click', (event) => {
         event.preventDefault();
         const request = new XMLHttpRequest();
         const formData = new FormData(form);
         request.onload = () => {
-            console.log(request.response);
+            if(request.status === 201) {
+                organisationSaveResultDOMElement.innerHTML = 'Dodano';
+                organisationSaveResultDOMElement.classList.remove('alert-danger');
+                organisationSaveResultDOMElement.classList.add('alert-info');
+                skillFormDOMElement.reset();
+            } else if(request.status < 500) {
+                organisationSaveResultDOMElement.innerHTML = 'Błędne dane!';
+                organisationSaveResultDOMElement.classList.add('alert-danger');
+                organisationSaveResultDOMElement.classList.remove('alert-info');
+            } else {
+                organisationSaveResultDOMElement.innerHTML = 'Błąd serwera!';
+                organisationSaveResultDOMElement.classList.add('alert-danger');
+                organisationSaveResultDOMElement.classList.remove('alert-info');
+            }
         }
         if (organisation_id){
             formData.append('organisation_id', organisation_id);
