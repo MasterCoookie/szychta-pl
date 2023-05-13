4b4a27523362f4ccee2ec37c25b16d48bbdfdef7
WojciechPtas <wojciech.ptas@gmail.com>
2023-05-13 14:01:38 +0200

    added space

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -14,7 +14,7 @@ router.post('/delete_organisation', authMiddleware.require_admin, adminControlle
 router.put('/add_employer', [authMiddleware.require_admin, upload.none()], adminController.addEmployer_put);
 router.post('/modify_employer', authMiddleware.require_admin, adminController.modifyEmployer_post);
 router.get('/manage_employer', authMiddleware.require_admin, adminController.manageEmployer_get);
-router.post('/delete_employer', [authMiddleware.require_admin,upload.none()], adminController.employer_delete);
+router.post('/delete_employer', [authMiddleware.require_admin, upload.none()], adminController.employer_delete);
 
 router.get('/adminPanel', authMiddleware.require_admin, adminController.panel_get);
 router.get('/show_organisations', adminController.show_organisations);
